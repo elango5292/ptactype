773436a7c7e24269a3183886f15e90c191a45306
macbook <macbook@MacBook-Air-2.local>
2024-11-14 20:42:27 +0530

    add tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics gaId="G-CKVVZY1S23" />
         {children}
       </body>
     </html>
